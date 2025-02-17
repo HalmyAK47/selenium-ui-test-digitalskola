4dce5d43dd62ff98235153ce7516dbbdca756610
HalmyAK47 <halmykhozaldy@gmail.com>
2025-02-17 15:39:48 +0700

    Add files via upload

---
+++ Selenium project/test.js
@@ -0,0 +1,130 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const assert = require("assert");
+
+// require("chromedriver");
+// const chrome = require("selenium-webdriver/chrome")
+
+async function slowType(element, text, delay = 100) {
+    for (let char of text) {
+        await element.sendKeys(char);
+        await new Promise(resolve => setTimeout(resolve, delay)); // Delay tiap karakter
+    }
+}
+
+async function exampleTest () {
+    //Membuat koneksi dengan webdriver menggunakan chrome
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    //menambahkan chrome option sebagai user agent menyerupai browser asli
+    //let option = new chrome.Options();
+
+    //perlu menambahkan argument juga
+    // options.addArguments("user-agent=Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166");
+    // options.addArguments("--disable-blink-features=AutomationControlled");
+
+    //perlu membuat koneksi dengan driver juga
+    // let driver = new Builder()
+    // .forBrowser("chrome")
+    // .setChromeOptions(options)
+    // .build();
+
+    //Exeception Handling & Conclusion
+
+    try {
+
+        // Maksimalkan window untuk dalam layar penuh
+        //await driver.manage().window().maximize();
+
+        //membuka url di browser
+        await driver.get("https://saucedemo.com");
+
+        //Melakukan sign in
+        let usernameField = await driver.findElement(By.xpath("//input[@id='user-name']"));
+        await slowType(usernameField, "standard_user", 100);
+
+        let passwordField = await driver.findElement(By.xpath("//input[@id='password']"));
+        await slowType(passwordField, "secret_sauce", 100);
+
+        //Klik tombol login
+        await driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        await driver.sleep(2000);
+
+        // Validasi masuk ke halaman dashboard setelah login
+        let currentUrl = await driver.getCurrentUrl();
+        assert.strictEqual(currentUrl, 'https://www.saucedemo.com/inventory.html',true, 'User tidak berada di halaman dashboard setelah login');
+
+        //Klik tombol add to cart
+        await driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        await driver.sleep(2000);
+
+        // Validasi produk ditambahkan menjadi 1
+        let totalitem = await driver.findElement(By.css(".shopping_cart_badge")).getText();
+        assert.strictEqual(totalitem.includes("1"),true,'Product berhasil ditambahkan menjadi 1"');
+
+        await driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']")).click();
+        await driver.sleep(1000);
+
+        // Validasi produk ditambahkan menjadi 2
+        totalitem = await driver.findElement(By.css(".shopping_cart_badge")).getText();
+        assert.strictEqual(totalitem.includes("2"),true,'Product berhasil ditambahkan menjadi 2"');
+        
+        await driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']")).click();
+        await driver.sleep(1000);
+
+        // Validasi produk ditambahkan menjadi 3
+        totalitem = await driver.findElement(By.css(".shopping_cart_badge")).getText();
+        assert.strictEqual(totalitem.includes("3"),true,'Product berhasil ditambahkan menjadi 3"');
+
+        await driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-fleece-jacket']")).click();
+        await driver.sleep(1000);
+
+        // Validasi produk ditambahkan menjadi 4
+        totalitem = await driver.findElement(By.css(".shopping_cart_badge")).getText();
+        assert.strictEqual(totalitem.includes("4"),true,'Product berhasil ditambahkan menjadi 4"');
+        
+        await driver.findElement(By.xpath("//div[@id='shopping_cart_container']/a[1]")).click();
+        await driver.sleep(2000);
+
+        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        await driver.sleep(2000);
+
+        await driver.findElement(By.xpath("//button[@id='checkout']")).click();
+        await driver.sleep(2000);
+
+        let First = await driver.findElement(By.xpath("//input[@id='first-name']"));
+        await slowType(First, "Halmy", 100);
+
+        let Last = await driver.findElement(By.xpath("//input[@id='last-name']"));
+        await slowType(Last, "AK", 100);
+        
+        let Zip = await driver.findElement(By.xpath("//input[@id='postal-code']"));
+        await slowType(Zip, "1774", 100);
+        await driver.sleep(2000);
+
+        await driver.findElement(By.xpath("//input[@id='continue']")).click();
+        await driver.sleep(2000);
+        
+        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        await driver.sleep(2000);
+
+        await driver.findElement(By.xpath("//button[@id='finish']")).click();
+        await driver.sleep(2000);
+
+        await driver.findElement(By.xpath("//button[@id='back-to-products']")).click();
+        await driver.sleep(4000);
+
+        //simulate user behavior typing hello world
+        // await searchBox.sendKeys("Hello World", Key.RETURN)
+        // await driver.wait(until.elementLocated(By.id(result-state)), 10000) //menunggu 10 detik
+
+        // let title = await driver.getTitle()
+        // console.log(`Page title is : ${title}`)
+
+    } finally {
+
+    await driver.quit();
+
+    }
+}
+
+exampleTest();
